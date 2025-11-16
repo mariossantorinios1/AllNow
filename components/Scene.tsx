a0553a676a331c1947eded400b8c22610b32b538
mariossantorinios1 <mariossantorinios71@gmail.com>
2025-11-16 14:44:51 +0200

    Update Scene.tsx

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -5,14 +5,18 @@ import { OrbitControls } from "@react-three/drei";
 
 export default function Scene() {
   return (
-    <div className="w-full h-[400px]">
-      <Canvas>
-        <OrbitControls />
+    <div className="w-full h-full">
+      <Canvas camera={{ position: [0, 0, 4] }}>
         <ambientLight intensity={0.5} />
+        <directionalLight position={[3, 3, 3]} />
+
+        {/* Simple cube so deployment never breaks */}
         <mesh>
           <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="orange" />
+          <meshStandardMaterial color={"hotpink"} />
         </mesh>
+
+        <OrbitControls enableZoom />
       </Canvas>
     </div>
   );
